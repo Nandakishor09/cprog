f8cee6c612aa77cdd9af7f639ca2d9c762d02dab
Nandakishor09 <nanda.v.kishor@gmail.com>
2025-08-10 13:28:37 +0530

    add code for game

---
+++ profit_loss.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int sp, cp;
+    printf("ENTER SELL PRICE: ");
+    scanf("%d", &sp);
+    printf("ENTER COST PRICE: ");
+    scanf("%d", &cp);
+
+    if (sp > cp){
+        printf("PROFIT IS %d", sp - cp);
+    }
+    else if( cp > sp){
+        printf("LOSE IS %d", cp - sp);
+    }
+    else{
+
+        printf("NO PROFIT OR LOSS");
+    }
+    return 0;
+
+
+
+}
