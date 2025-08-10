107725bc70cfe1f434daafc427ba2cd3fe5b1a9c
Nandakishor09 <nanda.v.kishor@gmail.com>
2025-08-10 12:27:25 +0530

    add code for game

---
+++ same_sign.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int num1, num2;
+    printf("ENTER NUM1: ");
+    scanf("%d", &num1);
+    printf("ENTER NUM2: ");
+    scanf("%d", &num2);
+
+    if( (num1&&num2 > 0) || (num1&&num2 <0) ){
+            printf("BOTH NUM1 AND NUM2 HAVE SAME SIGN");
+
+    }
+    else{
+        printf("NUM1 AND NUM2 DO NOT HAVE SAME SIGN");
+    }
+    return 0;
+}
