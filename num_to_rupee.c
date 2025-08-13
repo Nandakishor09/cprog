d575c888ecc7857e6f3182ddc3b7de200efab65a
Nandakishor09 <nanda.v.kishor@gmail.com>
2025-08-13 19:50:26 +0530

    Add files via upload

---
+++ num_to_rupee.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    int num, rem, quo;
+    printf("ENTER A NUMBER: ");
+    scanf("%d", &num);
+
+    rem = num % 100;
+    quo = num / 100;
+
+    printf("NUMBER OF 100 RUPEE NOTES = %d\n", quo);
+
+    if (rem > 50){
+
+        quo = rem / 50;
+        printf("NUMBER OF 50 RUPEE NOTES = %d\n", quo);
+
+        rem = rem % 50;
+
+        quo = rem / 10;
+        printf("NUMBER OF 10 RUPEE NOTES = %d", quo);
+
+
+
+    }
+
+    else{
+
+        printf("NUMBER OF 50 RUPEE NOTES IS 0\n");
+        quo = rem / 10;
+        printf("NUMBER OF 10 RUPEE NOTES = %d", quo);
+    }
+}
