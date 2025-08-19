4c6d9b1ddf415d5a603c641671364a9b2818463c
Nandakishor09 <nanda.v.kishor@gmail.com>
2025-08-19 19:22:05 +0530

    Update ticket_price.c

--- ticket_price.c
+++ ticket_price.c
@@ -8,36 +8,23 @@ int main(){
     printf("ENTER THE DAY OF MOVIE FOR ORDERS(1-7 WHERE 1 STANDS FOR SUNDAY): ");
     scanf("%d", &day);
 
-    if( day == 4){
-        if ( age < 12){
-            price = 100;
-            printf("YOUR TICKET PRICE: %d", price - 20);
-        }
-        else if( age >= 12 && age <= 60){
-            price = 150;
-            printf("YOUR TICKET PRICE: %d", price - 30);
-        }
-        else{
-            price = 120;
-            printf("YOUR TICKET PRICE: %d", price - 24);
-        }
-
+    if (age < 12){
+        price = 100;
+    }
+    else if (age >=12 && age <60){
+        price = 150;
     }
     else{
-        if ( age < 12){
-            price = 100;
-            printf("YOUR TICKET PRICE: %d", price);
-        }
-        else if( age >= 12 && age <= 60){
-            price = 150;
-            printf("YOUR TICKET PRICE: %d", price);
-        }
-        else{
-            price = 120;
-            printf("YOUR TICKET PRICE: %d", price);
-        }
-
+        price = 120;
     }
+    if (day == 4){
+        price = price - 20;
+    }
+    printf("your ticket price: %d", price);
+    return 0;
+
+    
     return 0;
 
 }
+
