eb582a00029cd88bb29e6a6178b72aa25ac0616c
Nandakishor09 <nanda.v.kishor@gmail.com>
2025-09-06 10:57:20 +0530

    Update prime_num.c

--- prime_num.c
+++ prime_num.c
@@ -20,5 +20,19 @@ int main(){
             printf("%d ", i);
         }
      }
-
+/* 
+for ( int i = 2; i <= num; i++){
+        int count = 0;
+        for ( int j = 2; j <= i; j++){
+            if ( i % j == 0){
+                //printf("%d ", i);
+                count++;
+            }
+        }
+        if ( count == 1){
+            printf("%d ", i);
+        }
+     }
+*/
 }
+
